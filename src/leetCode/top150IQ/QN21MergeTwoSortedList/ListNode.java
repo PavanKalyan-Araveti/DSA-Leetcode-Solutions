ca66a27b8bbc9857bf68635489dfb071c79e99c2
Pavan <pavankalyan.Araveti1999@gmail.com>
2025-08-13 23:14:09 +0530

    Solved One Problem

---
+++ src/leetCode/top150IQ/QN21MergeTwoSortedList/ListNode.java
@@ -0,0 +1,58 @@
+package leetCode.top150IQ.QN21MergeTwoSortedList;
+
+
+
+ // Definition for singly-linked list.
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+class Solution {
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null && list2 == null) return null;
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+        ListNode head;
+        if(list1.val <= list2.val){
+            head = list1;
+            list1 = list1.next;
+        }
+        else{
+            head = list2;
+            list2 = list2.next;
+        }
+        ListNode current = head;
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                current.next = list1;
+                list1 = list1.next;
+            }
+            else{
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+        current.next = (list1 != null) ? list1 : list2;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode list1 = null;
+        ListNode list2 = new ListNode(0);
+        ListNode merged = mergeTwoLists(list1, list2);
+        printList(merged);
+    }
+
+    public static void printList(ListNode head){
+        while(head != null){
+            System.out.print(head.val+" ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+}
---
+++ src/leetCode/top150IQ/QN56MergeIntervals/Solution.java
@@ -0,0 +1,4 @@
+package leetCode.top150IQ.QN56MergeIntervals;
+
+public class Solution {
+}
